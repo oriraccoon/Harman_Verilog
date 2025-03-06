f412482cc1462ff2539b41a4f18102bae243e654
kccistc <djdjskskalal@gmail.com>
2025-03-06 14:30:16 +0900

    Auto commit after Vivado closed

--- commit.bat
+++ commit.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Xilinx\Vivado\2020.2\bin\unwrapped\win64.o\vvgl.exe"   # Vivado 경로
+start "" "C:\Xilinx\Vivado\2020.2\bin\unwrapped\win64.o\vvgl.exe"
 powershell -ExecutionPolicy Bypass -File "C:\Users\kccistc\Documents\GitHub\Harman_Verilog\post_commit.ps1"
